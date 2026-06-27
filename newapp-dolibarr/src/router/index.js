2d837dd5a1e1662e99b0fb7f37dcd200223057f1
ItokianaRdrsn <anjaitokianaran@gmail.com>
2026-06-27 13:04:42 +0300

    OKEY

--- newapp-dolibarr/src/router/index.js
+++ newapp-dolibarr/src/router/index.js
@@ -1,10 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: "home",
+    component: () => import('@/views/Home.vue'),
+  },
+   {
+    path: '/backoffice/dashboard',
+    name: "backoffice-dashboard",
+    component: () => import('@/views/backoffice/DashboardView.vue'),
+    meta: { requiresBackoffice: true },
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 
+router.beforeEach((to) => {
+  const isBackofficeAuthenticated =
+    localStorage.getItem('backoffice_authenticated') === 'true'
+
+  if (to.meta.requiresBackoffice && !isBackofficeAuthenticated) {
+    return '/'
+  }
+})
 
 export default router
+
