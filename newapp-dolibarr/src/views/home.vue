d30322f589453961268b80f0a1ea406b75051c20
ItokianaRdrsn <anjaitokianaran@gmail.com>
2026-06-29 10:31:27 +0300

    link

--- newapp-dolibarr/src/views/home.vue
+++ newapp-dolibarr/src/views/home.vue
@@ -3,6 +3,8 @@
         <h1>Welcome to the Home Page</h1>
         <p>This is the home page of the application.</p>
         <RouterLink to="/backoffice/login">Go to Backoffice Login</RouterLink>
+        <p>Welcome to the Home Page! This is the main landing page of the application.</p>
+        <RouterLink :to="{ name: 'frontoffice-salaries' }">Go to Frontoffice Salaries</RouterLink>
     </div>
 </template>
 <script setup>
